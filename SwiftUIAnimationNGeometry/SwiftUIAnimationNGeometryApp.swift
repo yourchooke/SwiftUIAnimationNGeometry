ef173a7a4c85e9b83bf3d85f12a3892206d5a2c2
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-11-25 13:07:30 +0700

    Initial Commit

---
+++ SwiftUIAnimationNGeometry/SwiftUIAnimationNGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationNGeometryApp.swift
+//  SwiftUIAnimationNGeometry
+//
+//  Created by Olga Yurchuk on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationNGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
